bcf0c816c86dd233a4f9e77db8603398739884df
Andy MacKay <binarygeometry@gmail.com>
2024-11-29 14:58:51 +0000

    q2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- https://fonts.google.com/specimen/Roboto -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link  rel="preconnect" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     
     <style>
         <?php echo file_get_contents('./dist/css/critical.min.css'); ?>
@@ -18,11 +18,11 @@
 </head>
 <body>
     <div class="container">
-        <img src="./dist/images/logo.svg" alt="Logo" loading="lazy">
+        <img src="./dist/images/logo.svg" alt="Logo" loading="lazy" >
     
         <h1>Developer Test</h1>
     
-        <img class="hero" src="./dist/images/image-1.jpg" alt="Gaming Accessories" loading="lazy">
+        <img class="hero" src="./dist/images/image-1.jpg" alt="Gaming Accessories" loading="lazy" height="675" width="1200">
 
         <h2>Lorem ipsum dolor</h2>
 
